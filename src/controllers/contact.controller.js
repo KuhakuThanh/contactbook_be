f8ed00317f67630e3cdaf444e7673b5710a50bb4
ThanhB1910702 <thanhB1910702@student.ctu.edu.vn>
2022-11-20 15:40:10 +0700

    Define routes for managing contacts

---
+++ src/controllers/contact.controller.js
@@ -0,0 +1,21 @@
+exports.create = function (req,res,next) {
+    res.send({ message: 'create success!'});
+}
+exports.findAll = function (req,res,next) {
+    res.send({ message: 'findAll success!'});
+}
+exports.findOne = function (req,res,next) {
+    res.send({ message: 'findOne success!'});
+}
+exports.update = function (req,res,next) {
+    res.send({ message: 'update success!'});
+}
+exports.delete = function (req,res,next) {
+    res.send({ message: 'delete success!'});
+}
+exports.deleteAll = function (req,res,next) {
+    res.send({ message: 'deleteAll success!'});
+}
+exports.findAllFavorite = function (req,res,next) {
+    res.send({ message: 'findAllFavorite success!'});
+}
